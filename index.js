aa04b65e67f12ec09ad1570c4f8039bdaab26988
Sofia Klimova <klimovasonya@gmail.com>
2022-02-13 12:30:00 +0300

    fix: рефакторинг после ревью от 13.01

--- index.js
+++ index.js
@@ -1,60 +1,31 @@
+//попапы
 const editPopup = document.querySelector('.popup_type_edit');
-const popupToggles = document.querySelectorAll('.popup__toggle');
-const editButton = document.querySelector('.profile__edit-button');
-const submitButton = document.querySelector('.popup__button');
-const formPopup = document.querySelector('.popup__form');
-const cardsContainer = document.querySelector('.cards-grid__list');
-const addButton = document.querySelector('.profile__add-button');
 const addPopup = document.querySelector('.popup_type_add');
 const imagePopup = document.querySelector('.popup_type_image');
 
-//открытие попапа
-function openPopup(popup) {
-    popup.classList.add('popup_opened');
-}
+//кнопки
+const popupToggles = document.querySelectorAll('.popup__toggle');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const submitButton = document.querySelector('.popup__button');
 
-editButton.addEventListener('click', () => openPopup(editPopup));
-addButton.addEventListener('click', () => openPopup(addPopup));
-
-//закрытие попапа по крестику
-popupToggles.forEach(toggle =>
-    toggle.addEventListener('click', evt =>
-        evt.target.closest('.popup').classList.remove('popup_opened'))
-);
-
-//закрытие попапа после нажатия Submit или Enter
-function closePopup(popupName) {
-    popupName.classList.remove('popup_opened');
-}
-
-//открытие попапа с фотографией
-function openImagePopup(imageLink, header) {
-    const popupImage = imagePopup.querySelector('.popup__image');
-    popupImage.setAttribute('src', imageLink);
-    popupImage.setAttribute('alt', header);
-    imagePopup.querySelector('.popup__image-capture').textContent = header;
-    openPopup(imagePopup);
-}
-
-//поля формы редактирования заполнены по умолчанию
+//содержимое форм и попапов
+const formPopup = document.querySelector('.popup__form');
+const popupInput = addPopup.querySelectorAll('.popup__form-item');
 const nameInput = editPopup.querySelector('#name');
 const bioInput = editPopup.querySelector('#bio');
+const nameSubmit = addPopup.querySelector('#place');
+const linkSubmit = addPopup.querySelector('#link');
+const popupImage = imagePopup.querySelector('.popup__image');
+const imageCapture = imagePopup.querySelector('.popup__image-capture');
 
+//поля профиля
 const profileName = document.querySelector('.profile__name');
 const profileBio = document.querySelector('.profile__bio');
 
-nameInput.value = profileName.textContent;
-bioInput.value = profileBio.textContent;
-
-//редактирование информации профиля
-function editProfileInfo(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileBio.textContent = bioInput.value;
-    closePopup(editPopup);
-}
-
-formPopup.addEventListener('submit', editProfileInfo);
+//поля карточек
+const cardTemplate = document.querySelector('.cards-grid__template').content;
+const cardsContainer = document.querySelector('.cards-grid__list');
 
 //карточки
 const initialCards = [
@@ -84,9 +55,60 @@ const initialCards = [
     }
 ];
 
-//добавление карточек
-function addInitialCard(name, link) {
-    const cardTemplate = document.querySelector('.cards-grid__template').content;
+//открытие попапа
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+
+//закрытие попапа по крестику
+popupToggles.forEach(toggle =>
+    toggle.addEventListener('click', evt => {
+        evt.target.closest('.popup').classList.remove('popup_opened');
+    })
+);
+
+//закрытие попапа после нажатия Submit или Enter
+function closePopup(popupName) {
+    popupName.classList.remove('popup_opened');
+}
+
+//очищение полей для дальнейшего нового ввода
+function resetInput () {
+    popupInput.forEach(item => item.value = '');
+}
+
+//функционал попапа редактирования профиля
+editButton.addEventListener('click', () => {
+    openPopup(editPopup);
+    //поля формы редактирования заполнены по умолчанию
+    nameInput.value = profileName.textContent;
+    bioInput.value = profileBio.textContent;
+});
+
+//открытие попапа добавления карточки
+addButton.addEventListener('click', () => openPopup(addPopup));
+
+//функционал попапа с фотографией
+function openImagePopup(imageLink, header) {
+    popupImage.setAttribute('src', imageLink);
+    popupImage.setAttribute('alt', header);
+    imageCapture.textContent = header;
+    openPopup(imagePopup);
+}
+
+//редактирование информации профиля
+function editProfileInfo(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileBio.textContent = bioInput.value;
+    closePopup(editPopup);
+}
+
+//созранение информации из попапа редактирования
+formPopup.addEventListener('submit', editProfileInfo);
+
+//создание карточек
+function createCard(name, link) {
     const cardElement = cardTemplate.querySelector('.cards-grid__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.cards-grid__image');
     cardElement.querySelector('.cards-grid__title').textContent = name;
@@ -105,17 +127,17 @@ function addInitialCard(name, link) {
     cardElement.querySelector('.cards-grid__delete-card').addEventListener('click', function (evt) {
         evt.target.closest('.cards-grid__item').remove();
     })
-    cardsContainer.prepend(cardElement);
+
+    return cardElement;
 }
 
 //добавление карточек из initialcards
-initialCards.forEach(item => addInitialCard(item.name, item.link));
+initialCards.forEach(item => cardsContainer.append(createCard(item.name, item.link)));
 
 //добавление новой карточки через форму
-addPopup.addEventListener('submit', (evt, nameSubmit, linkSubmit) => {
+addPopup.addEventListener('submit', evt => {
     evt.preventDefault();
-    nameSubmit = addPopup.querySelector('#place');
-    linkSubmit = addPopup.querySelector('#link');
-    addInitialCard(nameSubmit.value, linkSubmit.value);
+    cardsContainer.prepend(createCard(nameSubmit.value, linkSubmit.value));
     closePopup(addPopup);
+    resetInput();
 })
